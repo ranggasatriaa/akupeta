5b2e74402963c9979cc6003fe653bac0db6ad297
Rangga Satria A <ranggasatriaa@gmail.com>
2018-07-31 19:58:31 +0700

    Initial commit

---
+++ app/src/main/java/com/astagenta/rangga/akupeta/MapsActivity.java
@@ -0,0 +1,232 @@
+package com.astagenta.rangga.akupeta;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.squareup.picasso.Picasso;
+import com.squareup.picasso.RequestCreator;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private RequestQueue mRequestQueue;
+    private TextView mText;
+
+//    private ArrayList<MarkerData> markersArray = new ArrayList<MarkerData>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        mText = findViewById(R.id.text_view_result);
+        mRequestQueue = Volley.newRequestQueue(this);
+
+        //meminta permission user untuk mengakses lokasi
+        //untuk versi marshmallow keatas butuh penanganan permission yang berbeda dari versi sebelumnya
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            checkLocationPermission();
+        }
+
+
+
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    //kode untuk permision mengakses lokasi
+    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
+
+    public void checkLocationPermission(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            // Asking user if explanation is needed
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+
+                //Prompt the user once explanation has been shown
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+        }
+    }
+
+    //menyimpan permission yang diizinkan oleh user
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    // permission was granted. Do the
+                    // contacts-related task you need to do.
+                    if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
+                        mMap.setMyLocationEnabled(true);
+                    }
+                } else {
+
+                    // Permission denied, Disable the functionality that depends on this permission.
+                    Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+
+            // other 'case' lines to check for other permissions this app might request.
+            // You can add here other case statements according to your requirement.
+        }
+    }
+
+    @Override
+    //pemanggilan tampilan pada google maps
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        //menambahkan tombol untuk menuju lokasi sekarang
+
+        //permisalan untuk android marsmellow
+        if (android.os.Build.VERSION .SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                mMap.setMyLocationEnabled(true);
+            }
+        }
+        else {
+            //android dibawah masrsmelow
+            mMap.setMyLocationEnabled(true);
+        }
+
+        Button buttonLocation = (Button) findViewById(R.id.btnLocation);
+        //listener ketika button di klik
+        buttonLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //ambil latitude dan longitude lokasi
+                LatLng center = mMap.getCameraPosition().target;
+                //method untuk mengambil alamat lokasi
+                getAddress(center.latitude,center.longitude);
+                //JSON PARSE
+                jsonParse();
+            }
+        });
+    }
+
+    public void getAddress(double lat, double lng) {
+        //mengonversi koordinat geografis yang akan ditempatkan pada peta jadi alamat.
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            //bentuk objek dari alamat
+            Address obj = addresses.get(0);
+            String add = obj.getAddressLine(0);
+            add = add + "\n" + obj.getCountryName();
+            add = add + "\n" + obj.getCountryCode();
+            add = add + "\n" + obj.getAdminArea();
+            add = add + "\n" + obj.getPostalCode();
+            add = add + "\n" + obj.getSubAdminArea();
+            add = add + "\n" + obj.getLocality();
+            add = add + "\n" + obj.getSubThoroughfare();
+
+            Toast.makeText(this, add, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void jsonParse() {
+        String url = "http://nearyou.ranggasatria.com/index.php/json/read";
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        mText.setText("");
+                        mMap.clear();
+                        try {
+                            JSONArray jsonArray = response.getJSONArray("tempat");
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject tempat = jsonArray.getJSONObject(i);
+                                String firstName = tempat.getString("tempat_nama");
+                                Double latitude = tempat.getDouble("tempat_latitude");
+                                Double longitude = tempat.getDouble("tempat_longitude");
+                                String imageUrl = tempat.getString("kategori_icon");
+
+                                mText.append(firstName + ", "+ latitude + ", " + longitude + ", " + imageUrl + "\n\n");
+                                createMarker(latitude, longitude,firstName, imageUrl);
+
+//                                Picasso.with(MapsActivity.this).load("http://nearyou.ranggasatria.com/assets/"+imageUrl).fit().centerInside().into(mImage);
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                , new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        mRequestQueue.add(request);
+    }
+
+    protected Marker createMarker(double latitude, double longitude, String title, String snippet) {
+
+//        RequestCreator image = Picasso.with(MapsActivity.this).load("http://nearyou.ranggasatria.com/assets/img/map-marker.png");
+
+        return mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(latitude, longitude))
+                .anchor(0.5f, 0.5f)
+                .title(title)
+                .snippet(snippet)
+//                .icon(BitmapDescriptorFactory.fromBitmap(image))
+        );
+    }
+}
