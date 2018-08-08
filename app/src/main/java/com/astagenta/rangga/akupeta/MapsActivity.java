01149a48d2322df5443c174c911a9542061099d4
Unknown <ranggasatriaa@gmail.com>
2018-08-08 20:24:44 +0700

    lanjutin commit sebelumnya

--- app/src/main/java/com/astagenta/rangga/akupeta/MapsActivity.java
+++ app/src/main/java/com/astagenta/rangga/akupeta/MapsActivity.java
@@ -2,6 +2,7 @@ package com.astagenta.rangga.akupeta;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.location.Address;
@@ -26,9 +27,9 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -59,11 +60,14 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final int DEFAULT_ZOOM = 15;
     public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
 
+    //kode untuk permision mengakses lokasi
+    private FusedLocationProviderClient mFusedLocationProviderClient;
     private Boolean mLocationPermissionGranted = false;
     private GoogleMap mMap;
     private RequestQueue mRequestQueue;
     private TextView mText;
-    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private Context context;
 
 
     @Override
@@ -73,62 +77,88 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mText = findViewById(R.id.text_view_result);
         mRequestQueue = Volley.newRequestQueue(this);
 
-        getLocationPermission();
-
-        //meminta permission user untuk mengakses lokasi
-        //untuk versi marshmallow keatas butuh penanganan permission yang berbeda dari versi sebelumnya
+        /*meminta permission user untuk mengakses lokasi untuk versi marshmallow keatas butuh penanganan permission yang berbeda dari versi sebelumnya*/
         if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             checkLocationPermission();
         }
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
+        getDeviceLocation();
+
+        /*Obtain the SupportMapFragment and get notified when the map is ready to be used.*/
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
-    public void checkLocationPermission(){
+
+    public void checkLocationPermission() {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
-            // Asking user if explanation is needed
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
-                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+            /* Asking user if explanation is needed*/
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
 
-                // Show an explanation to the user *asynchronously* -- don't block
-                // this thread waiting for the user's response! After the user
-                // sees the explanation, try again to request the permission.
+                /* Prompt the user once explanation has been shown*/
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                Log.d(TAG, "checkLocationPermission: jika");
 
-                //Prompt the user once explanation has been shown
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
             } else {
-                // No explanation needed, we can request the permission.
-                ActivityCompat.requestPermissions(this,
-                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
-                        MY_PERMISSIONS_REQUEST_LOCATION);
+                /* No explanation needed, we can request the permission.*/
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, MY_PERMISSIONS_REQUEST_LOCATION);
+                Log.d(TAG, "checkLocationPermission: else");
             }
         }
     }
 
-    //menyimpan permission yang diizinkan oleh user
+    private void getDeviceLocation() {
+        Log.d(TAG, "getDeviceLocation: Getting device current location");
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        Task location = mFusedLocationProviderClient.getLastLocation();
+        location.addOnCompleteListener(new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "onComplete:  found location");
+                    Location curretLocation = (Location) task.getResult();
+                    Log.d(TAG, "onComplete: Lat: " + curretLocation.getLatitude() + "Long: " + curretLocation.getLongitude());
+                    moveCamera(new LatLng(curretLocation.getLatitude(), curretLocation.getLongitude()), DEFAULT_ZOOM);
+                } else {
+                    Log.d(TAG, "onComplete: current location is null");
+                    Toast.makeText(MapsActivity.this, "unnable to get curent location", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    /*MERUBAH CENTER PETA*/
+    private void moveCamera(LatLng latLng, float zoom) {
+        Log.d(TAG, "MoveCamera: moving camera to lat: " + latLng.latitude + " long " + latLng.longitude);
+//        Toast.makeText(this, "lat: "+latLng.latitude+" long: "+latLng.longitude+"zoom: "+zoom,Toast.LENGTH_SHORT).show();
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    /*menyimpan permission yang diizinkan oleh user*/
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
             case MY_PERMISSIONS_REQUEST_LOCATION: {
-                // If request is cancelled, the result arrays are empty.
+                /*If request is cancelled, the result arrays are empty.*/
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    // permission was granted. Do the
-                    // contacts-related task you need to do.
+                    /* permission was granted. Do the*/
+                    /* contacts-related task you need to do.*/
                     if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                         mMap.setMyLocationEnabled(true);
+                        getDeviceLocation();
                     }
                 } else {
-                    // Permission denied, Disable the functionality that depends on this permission.
+                    /*Permission denied, Disable the functionality that depends on this permission.*/
                     Toast.makeText(this, "permission denied", Toast.LENGTH_LONG).show();
                 }
                 break;
             }
-            // other 'case' lines to check for other permissions this app might request.
-            // You can add here other case statements according to your requirement.
         }
     }
 
@@ -136,107 +166,81 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     /*pemanggilan tampilan pada google maps*/
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
-        //menambahkan tombol untuk menuju lokasi sekarang
-
+//  mMap.setMapStyle(GoogleMap.MAP_TYPE_NONE);
         //permisalan untuk android marsmellow
-        if (android.os.Build.VERSION .SDK_INT >= Build.VERSION_CODES.M) {
+        if (android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
                 mMap.setMyLocationEnabled(true);
-//                mMap.getUiSettings().setMyLocationButtonEnabled(true);
-//                mMap.setMyLocationEnabled(true);
             }
-        }
-        else {
-            /*android dibawah masrsmelow*/
+        } else {
+            //android dibawah masrsmelow
             mMap.setMyLocationEnabled(true);
         }
+        /*JSON PARSE*/
+        jsonParse("http://nearyou.ranggasatria.com/index.php/json/read");
 
-//        LatLng latLng = new LatLng(-7.0527812, 110.3876632);
-//        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 10);
-//        mMap.animateCamera(cameraUpdate);
-
-        Button buttonLocation = (Button) findViewById(R.id.btnLocation);
-        //listener ketika button di klik
-        buttonLocation.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //ambil latitude dan longitude lokasi
-                LatLng center = mMap.getCameraPosition().target;
-                //method untuk mengambil alamat lokasi
-                getAddress(center.latitude,center.longitude);
-                //JSON PARSE
-                jsonParse("http://nearyou.ranggasatria.com/index.php/json/read");
-            }
-        }else{
-            ActivityCompat.requestPermissions(this, permissions, LOCATION_PERMISSION_REQUEST_CODE);
-        }
+//    Button buttonLocation = (Button) findViewById(R.id.btnLocation);
+//    //listener ketika button di klik
+//    buttonLocation.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View v) {
+//        //ambil latitude dan longitude lokasi
+//        LatLng center = mMap.getCameraPosition().target;
+//        //method untuk mengambil alamat lokasi
+//        getAddress(center.latitude, center.longitude);
+//        //JSON PARSE
+//      }
+//    });
     }
 
-    private void initMap(){
-        Log.d(TAG, "initMap: initializing map");
-        Toast.makeText(this, "MAPS IS REAFY", Toast.LENGTH_SHORT).show();
+    public void getAddress(double lat, double lng) {
+        //mengonversi koordinat geografis yang akan ditempatkan pada peta jadi alamat.
+        Geocoder geocoder = new Geocoder(getApplicationContext(), Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            //bentuk objek dari alamat
+            Address obj = addresses.get(0);
+            String add = obj.getAddressLine(0);
+            add = add + "\n" + obj.getCountryName();
+            add = add + "\n" + obj.getCountryCode();
+            add = add + "\n" + obj.getAdminArea();
+            add = add + "\n" + obj.getPostalCode();
+            add = add + "\n" + obj.getSubAdminArea();
+            add = add + "\n" + obj.getLocality();
+            add = add + "\n" + obj.getSubThoroughfare();
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(MapsActivity.this);
-    }
-
-    private void getDeviceLocation(){
-        Log.d(TAG, "getDeviceLocation: Getting device current location");
-
-        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
-
-        try{
-            if (mLocationPermissionGranted){
-                Task location = mFusedLocationProviderClient.getLastLocation();
-                location.addOnCompleteListener(new OnCompleteListener() {
-                    @Override
-                    public void onComplete(@NonNull Task task) {
-                        if (task.isSuccessful()){
-                            Log.d(TAG, "onComplete:  found location");
-                            Location curretLocation = (Location) task.getResult();
-                            Log.d(TAG, "onComplete: Lat: "+curretLocation.getLatitude()+"Long: "+curretLocation.getLongitude());
-                            moveCamera(new LatLng(curretLocation.getLatitude(), curretLocation.getLongitude()), DEFAULT_ZOOM);
-                        }else{
-                            Log.d(TAG, "onComplete: current location is null");
-                            Toast.makeText(MapsActivity.this, "unnable to get curent location", Toast.LENGTH_SHORT).show();
-                        }
-
-                    }
-                });
-            }else{
-                Log.d(TAG, "getDeviceLocation: mLocationPermisionGranted: false");
-            }
-
-        }catch (SecurityException e){
-            Log.d(TAG, "getDeviceLocation: " + e.getMessage());
+            Toast.makeText(this, add, Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
     private void jsonParse(String url) {
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        mMap.clear();
-                        try {
-                            JSONArray jsonArray = response.getJSONArray("tempat");
-                            for (int i = 0; i < jsonArray.length(); i++) {
-                                JSONObject tempat = jsonArray.getJSONObject(i);
-                                String firstName = tempat.getString("tempat_nama");
-                                Double latitude = tempat.getDouble("tempat_latitude");
-                                Double longitude = tempat.getDouble("tempat_longitude");
-                                String imageUrl = tempat.getString("kategori_icon");
-
-                                createMarker(latitude, longitude,firstName, imageUrl);
-//                                Picasso.with(MapsActivity.this).load("http://nearyou.ranggasatria.com/assets/"+imageUrl).fit().centerInside().into(mImage);
-                            }
-                        } catch (JSONException e) {
-                            e.printStackTrace();
+            new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject response) {
+//                        mText.setText("");
+                    mMap.clear();
+                    try {
+                        JSONArray jsonArray = response.getJSONArray("Tempat");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject tempat = jsonArray.getJSONObject(i);
+                            String tempatId = tempat.getString("id");
+                            String firstName = tempat.getString("tempat_nama");
+                            Double latitude = tempat.getDouble("tempat_latitude");
+                            Double longitude = tempat.getDouble("tempat_longitude");
+                            String imageUrl = tempat.getString("kategori_icon");
+//              mText.append(firstName + ", "+ latitude + ", " + longitude + ", " + imageUrl + "\n\n");
+                            createMarker(latitude, longitude, tempatId, firstName, imageUrl);
+//              Picasso.with(MapsActivity.this).load("http://nearyou.ranggasatria.com/assets/"+imageUrl).fit().centerInside().into(mImage);
                         }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
                     }
                 }
-                , new Response.ErrorListener() {
+            }
+            , new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 error.printStackTrace();
@@ -246,13 +250,25 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mRequestQueue.add(request);
     }
 
-    protected Marker createMarker(double latitude, double longitude, String title, String snippet) {
+    protected Marker createMarker(double latitude, double longitude, String id, String title, String snippet) {
+//        RequestCreator image = Picasso.with(MapsActivity.this).load("http://nearyou.ranggasatria.com/assets/img/map-marker.png");
+        mMap.setOnInfoWindowClickListener(new GoogleMap.OnInfoWindowClickListener() {
+            @Override
+            public void onInfoWindowClick(Marker marker) {
+                Intent intent = new Intent(MapsActivity.this, DetailActivity.class);
+                intent.putExtra("TEMPAT_ID", "1");
+                context.startActivity(intent);
 
+            }
+        });
         return mMap.addMarker(new MarkerOptions()
                 .position(new LatLng(latitude, longitude))
                 .anchor(0.5f, 0.5f)
                 .title(title)
-                .snippet(snippet)
+                .snippet(snippet+" -> " +id)
+//          .icon(BitmapDescriptorFactory.fromBitmap(image))
         );
+
     }
+
 }
--- app/src/main/java/com/astagenta/rangga/akupeta/Tempat.java
+++ app/src/main/java/com/astagenta/rangga/akupeta/Tempat.java
@@ -1,4 +1,115 @@
 package com.astagenta.rangga.akupeta;
 
 public class Tempat {
+
+  package com.astagenta.rangga.akupeta;
+
+import android.content.Intent;
+
+  public class Tempat {
+    private int id;
+    private String nama;
+    private String deskripsi;
+    private String telphone;
+    private Double latitude;
+    private Double longitude;
+    private Integer kategoriId;
+    private Integer status;
+    private String kategoriName;
+    private String kategoriIcon;
+
+    public Tempat(int id, String nama, String deskripsi, String telphone, Double latitude, Double longitude, Integer kategoriId, Integer status, String kategoriName, String kategoriIcon) {
+      this.id = id;
+      this.nama = nama;
+      this.deskripsi = deskripsi;
+      this.telphone = telphone;
+      this.latitude = latitude;
+      this.longitude = longitude;
+      this.kategoriId = kategoriId;
+      this.status = status;
+      this.kategoriName = kategoriName;
+      this.kategoriIcon = kategoriIcon;
+    }
+
+    public int getId() {
+      return id;
+    }
+
+    public void setId(int id) {
+      this.id = id;
+    }
+
+    public String getNama() {
+      return nama;
+    }
+
+    public void setNama(String nama) {
+      this.nama = nama;
+    }
+
+    public String getDeskripsi() {
+      return deskripsi;
+    }
+
+    public void setDeskripsi(String deskripsi) {
+      this.deskripsi = deskripsi;
+    }
+
+    public String getTelphone() {
+      return telphone;
+    }
+
+    public void setTelphone(String telphone) {
+      this.telphone = telphone;
+    }
+
+    public Double getLatitude() {
+      return latitude;
+    }
+
+    public void setLatitude(Double latitude) {
+      this.latitude = latitude;
+    }
+
+    public Double getLongitude() {
+      return longitude;
+    }
+
+    public void setLongitude(Double longitude) {
+      this.longitude = longitude;
+    }
+
+    public Integer getKategoriId() {
+      return kategoriId;
+    }
+
+    public void setKategoriId(Integer kategoriId) {
+      this.kategoriId = kategoriId;
+    }
+
+    public Integer getStatus() {
+      return status;
+    }
+
+    public void setStatus(Integer status) {
+      this.status = status;
+    }
+
+    public String getKategoriName() {
+      return kategoriName;
+    }
+
+    public void setKategoriName(String kategoriName) {
+      this.kategoriName = kategoriName;
+    }
+
+    public String getKategoriIcon() {
+      return kategoriIcon;
+    }
+
+    public void setKategoriIcon(String kategoriIcon) {
+      this.kategoriIcon = kategoriIcon;
+    }
+  }
+
 }
